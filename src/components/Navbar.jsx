c79c21e994392baa1a1522c43ddbef8599500b5e
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-02-23 00:53:51 -0500

    updated to hide elements on shrink. Added mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,26 +19,71 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'flex',
+        }
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        }
+    },
 }));
 
 export default function Navbar() {
     const classes = useStyles();
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+    
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleMobileMenuOpen = (event) => { setMobileMoreAnchorEl(event.currentTarget); }
+
+    const handleMobileMenuClose = () => { setMobileMoreAnchorEl(null); }
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu anchorEl={mobileMoreAnchorEl} id={mobileMenuId} keepMounted
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMobileMenuOpen} onClose={handleMobileMenuClose}
+            >
+            <MenuItem>
+                <p>Skills</p>
+            </MenuItem>
+            <MenuItem>
+                <p>Projects</p>
+            </MenuItem>
+            <MenuItem>
+                <p>Contact</p>
+            </MenuItem>
+        </Menu>
+    );
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         Talha
                     </Typography>
-                    <Button color="inherit">Skills</Button>
-                    <Button color="inherit">Projects</Button>
-                    <Button color="inherit">Contact</Button>
+
+                    <div className={classes.sectionDesktop}>
+                        <Button color="inherit">Skills</Button>
+                        <Button color="inherit">Projects</Button>
+                        <Button color="inherit">Contact</Button>
+                    </div>
+
+                    <div className={classes.sectionMobile}>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" aria-controls={mobileMenuId} aria-haspopup="true" onClick={handleMobileMenuOpen}>
+                            <MenuIcon />
+                        </IconButton>
+                    </div>
                 </Toolbar>
             </AppBar>
+            {renderMobileMenu}
         </div>
     );
 }
