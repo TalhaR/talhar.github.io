63dd0244b4d57d0d59ba60c1d07485d40a8b2016
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-02-23 00:52:54 -0500

    moved css over to jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import stars from '../img/stars.jpg'
 import '../index.css'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+    headerImage: {
+        maxWidth: '100%'
+    },
+});
 
 export default function Header() {
+    const classes = useStyles();
+
     return (
         <React.Fragment>
             <header>
                 <Grid container justify="center">
                     <Grid item xs={12}>
-                        <img id="header-img" src={stars} alt="header" />
+                        <img className={classes.headerImage} src={stars} alt="header" />
                     </Grid>
                 </Grid>
             </header>
