4161664f0c4a9672a751dd42a17af8087ba564b4
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-03-10 15:05:32 -0500

    added raised property to cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles'
 import { Button, Card, CardActions, CardContent, CardMedia, Chip, Grid, Typography } from '@material-ui/core'
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
     cardContainer: {
+        // transition: 'transform .2s cubic-bezier(.34,2,.6,1),box-shadow .2s ease'
     },
     media: {
         paddingTop: '56.25%'
@@ -24,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
 
 function ProjectCard({title, img, desc, features, link, link2}) {
     const classes = useStyles();
+    const [raised, setRaised] = useState(false);
 
     const getFeatures = (featuresList) => {
         return featuresList.map((feature) => {
@@ -31,10 +34,12 @@ function ProjectCard({title, img, desc, features, link, link2}) {
         });
     }
 
+    const toggleRaise = () => setRaised(!raised);
+
     return (
         <Grid item xs={10} sm={9} lg={5} 
             style={{paddingBottom: '5%'}}>
-            <Card className={classes.cardContainer}>
+            <Card className={classes.cardContainer} onMouseOver={() => toggleRaise()} onMouseOut={() => toggleRaise()} raised={raised} >
                 <CardMedia className={classes.media} 
                 image={ img }/>
                 <CardContent>
