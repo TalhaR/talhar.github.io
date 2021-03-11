7a59ec062ffedd6aa675147e088461210b62dfa4
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-03-10 22:04:24 -0500

    fixed center issue on firefox

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,43 +1,49 @@
-import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Typography } from '@material-ui/core'
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     aboutContainer: {
-        textAlign: 'center',
-        justifyContent: 'center',
-        margin: '5% 0%',
-        [theme.breakpoints.up("xl")] : {
-            margin: '3% 0%'
-        }
+        textAlign: "center",
+        justifyContent: "center",
+        margin: "4% 0%",
+        [theme.breakpoints.up("xl")]: {
+            margin: "3% 0%",
+        },
     },
     aboutText: {
-        justifyContent: 'center',
-        textAlign: 'center',
-        width: '50%',
-        [theme.breakpoints.up('sm')]: {
-            margin: '1% 0%',
+        justifyContent: "center",
+        textAlign: "center",
+        [theme.breakpoints.up("sm")]: {
+            margin: "1% 0%",
         },
-    }
+    },
 }));
 
 export default function About() {
     const classes = useStyles();
 
     return (
-        <Grid container className={classes.aboutContainer} component="section">
-            <Grid item style={{textAlign: '-webkit-center'}}>
-                <Typography variant="h1">
-                    Talha Rahman
-                </Typography>
-                <br />
+        <Grid
+            container
+            className={classes.aboutContainer}
+            component="section"
+            spacing={3}
+        >
+            <Grid item xs={12}>
+                <Typography variant="h1">Talha Rahman</Typography>
+            </Grid>
+            <Grid item xs={12}>
                 <Typography variant="h2">
                     <small> Student Software Developer </small>
                 </Typography>
-                <br />
-                <Typography variant="body1" className={classes.aboutText}>
-                    I am a senior computer science major at Hunter College. I try my best to write code that runs without complier errors. Check more about me and some projects I am working on below.
+            </Grid>
+            <Grid item xs={12} sm={8} md={6} lg={4} className={classes.aboutText}>
+                <Typography variant="body1">
+                    I am a senior computer science major at Hunter College. I
+                    try my best to write code that runs without complier errors.
+                    Check more about me and some projects I am working on below.
                 </Typography>
             </Grid>
         </Grid>
-    )
+    );
 }
